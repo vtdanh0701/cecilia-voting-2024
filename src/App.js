ac286969450d86a830fddce0effe2d56c5d5dd1a
vtdanh0701 <vtdanh0701@gmail.com>
2024-01-10 07:23:27 -0800

    remove required fields, add minFieldError check

--- src/App.js
+++ src/App.js
@@ -1,67 +1,75 @@
 import React, { useState } from "react";
-import { Form, Button, Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Col, Row, Container, Alert } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import DescriptionTooltip from "./components/DescriptionTooltip.js";
 import descriptions from "./data/descriptions.js";
 import formElementArr from "./data/formElementArr.js";
-import "../src/App.css"
+import "../src/App.css";
 import { Amplify } from "aws-amplify";
-import config from './aws-exports.js'
-import { generateClient } from 'aws-amplify/api';
+import config from "./aws-exports.js";
+import { generateClient } from "aws-amplify/api";
 import { createVoting } from "./graphql/mutations.js";
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 import ConfirmModel from "./components/ConfirmModel.js";
 
-Amplify.configure(config)
+Amplify.configure(config);
 const client = generateClient();
-
 
 const App = () => {
     const [validated, setValidated] = useState(false);
-    const [showModel, setShowModel] = useState(false)
+    const [showModel, setShowModel] = useState(false);
+    const [isMinFieldError, setIsMinFieldError] = useState(false);
     const [data, setData] = useState();
-    const navigate = useNavigate()
+    const navigate = useNavigate();
 
-    const handleSubmit = event => {
+    const handleSubmit = (event) => {
         event.preventDefault();
         const form = event.currentTarget;
         setValidated(true);
-        if (form.checkValidity() === true) {
-            const formData = new FormData(form);
-            const dataObj = Object.fromEntries(formData.entries());
-            setData(dataObj); 
-            setShowModel(true); 
-        }
-        else{
-            event.stopPropagation();
-        }
-    }
 
-    const handleConfirmSubmit = async ()  => {
-        try{
+        const formData = new FormData(form);
+        const dataObj = Object.fromEntries(formData.entries());
+        const isAtLeastOneFieldFilled = Object.values(dataObj).some(
+            (value) => value.trim() !== ""
+        );
+
+        if (!isAtLeastOneFieldFilled) {
+            setIsMinFieldError(true);
+            event.stopPropagation();
+            return;
+        } else {
+            setIsMinFieldError(false);
+        }
+
+        if (form.checkValidity() === true) {
+            setData(dataObj);
+            setShowModel(true);
+        }
+    };
+
+    const handleConfirmSubmit = async () => {
+        try {
             let result = await client.graphql({
                 query: createVoting,
-                variables: {input: data}
-            })
-            console.log('result from graphQL', result)
-        } catch (e){
-            console.error('Error submitting', e.message);
+                variables: { input: data },
+            });
+        } catch (e) {
+            console.error("Error submitting", e.message);
             throw Error(e);
-        } finally{
-            setShowModel(false)
-            navigate('/thank-you')
+        } finally {
+            setShowModel(false);
+            navigate("/thank-you");
         }
     };
     return (
-        <div className="page-background">
-            <ConfirmModel 
-                showModel={showModel} 
-                setShowModel={setShowModel} 
-                handleConfirmSubmit={handleConfirmSubmit}/>
-            <Container
-                className='d-flex flex-column justify-content-center align-items-center form-text mobile-padding form-container'
-            >
+        <div className='page-background'>
+            <ConfirmModel
+                showModel={showModel}
+                setShowModel={setShowModel}
+                handleConfirmSubmit={handleConfirmSubmit}
+            />
+            <Container className='d-flex flex-column justify-content-center align-items-center form-text mobile-padding form-container'>
                 <h3
                     className='text-center mt-4 mb-4'
                     style={{ color: "#ff61a2" }}
@@ -99,20 +107,25 @@ const App = () => {
                                         </Row>
                                         <Col xs={12} lg={8}>
                                             <Form.Control
-                                                required
                                                 size='md'
                                                 type={e.type}
                                                 placeholder={e.placeholder}
                                                 pattern='^[A-Za-zÀ-ỹ ]+$'
                                                 name={e.name}
                                             />
-                                            <Form.Control.Feedback type='invalid'>
-                                                Please enter a valid username
-                                            </Form.Control.Feedback>
                                         </Col>
                                     </Form.Group>
                                 );
                             })}
+                            {isMinFieldError && (
+                                <Row className='mb-3'>
+                                    <Col xs={12}>
+                                        <Alert variant='danger'>
+                                            Please fill out at least one field.
+                                        </Alert>
+                                    </Col>
+                                </Row>
+                            )}
                             <Row>
                                 <Col className='text-center mt-2 mb-4'>
                                     <Button
